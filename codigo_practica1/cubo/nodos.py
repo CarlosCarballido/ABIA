6db60ed5a0bd381316d782a37ad6b5fce28ce03c
Carlos Carballido <carlos.carballido@alumnos.uvigo.es>
2024-04-08 02:12:55 +0200

    Refactor class `NodoProfundidad` to remove the `operador` attribute and fix typo in `heuristica` variable name in `NodoVoraz` class

--- codigo_practica1/cubo/nodos.py
+++ codigo_practica1/cubo/nodos.py
@@ -1,36 +1,38 @@
 class Nodo:
     def __init__(self, estado, padre):
-        self.estado=estado
-        self.padre=padre
+        self.estado = estado
+        self.padre = padre
 
 class NodoAnchura(Nodo):
     def __init__(self, estado, padre, operador):
         super().__init__(estado, padre)
-        self.operador=operador
+        self.operador = operador
 
 class NodoProfundidad(Nodo):
     def __init__(self, estado, padre, operador):
         super().__init__(estado, padre)
-        self.operador=operador
+        self.operador = operador
+
 class NodoProfundidadIterativa(Nodo):
     def __init__(self, estado, padre, operador, profundidad):
         super().__init__(estado, padre)
-        self.operador=operador
-        self.profundidad=profundidad
-class NodoVoraz:
+        self.operador = operador
+        self.profundidad = profundidad
+
+class NodoVoraz(Nodo):
     def __init__(self, estado, padre, operador, heuristica):
         super().__init__(estado, padre)
         self.operador = operador
         self.heuristica = heuristica
 
-class NodoAStar:
+class NodoAStar(Nodo):
     def __init__(self, estado, padre, operador, coste, heuristica):
         super().__init__(estado, padre)
         self.operador = operador
         self.coste = coste
         self.heuristica = heuristica
 
-class NodoIDAStar:
+class NodoIDAStar(Nodo):
     def __init__(self, estado, padre, operador, coste):
         super().__init__(estado, padre)
         self.operador = operador
